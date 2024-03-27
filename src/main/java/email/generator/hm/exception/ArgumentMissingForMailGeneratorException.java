19e53c2749a9c328d4846ca135eb6a9b11e0c0aa
LikaGlonti <likaglonti20@gmail.com>
2024-03-27 23:57:40 +0400

    Revert "Revert "throw ArgumentMissingForMailGeneratorException if arguments are missing""
    This reverts commit 8b99e84da260509cd493b2e29ae57881372dc3d1.

---
+++ src/main/java/email/generator/hm/exception/ArgumentMissingForMailGeneratorException.java
@@ -0,0 +1,7 @@
+package email.generator.hm.exception;
+
+public class ArgumentMissingForMailGeneratorException extends Exception {
+    public ArgumentMissingForMailGeneratorException (String errorMessage) {
+        super(errorMessage);
+    }
+}
