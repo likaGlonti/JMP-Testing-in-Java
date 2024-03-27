f04f87ecdc150fe0ed3ebe7fa34b073e898f043f
VGLONLI <lika.glonti@pearson.com>
2024-03-27 18:58:43 +0400

    add dynamic test for replacePlaceHoldersWithProvidedValue

--- src/test/java/email/generator/hm/EmailGeneratorTest.java
+++ src/test/java/email/generator/hm/EmailGeneratorTest.java
@@ -1,11 +1,17 @@
 package email.generator.hm;
 
+import email.generator.hm.mock.GeneratedEmailsOutputsMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 class EmailGeneratorTest {
@@ -13,7 +19,7 @@ class EmailGeneratorTest {
     @MethodSource("provideParameters")
     @DisplayName("should test if placeholders are replaced with provided values with Parametrized Tests")
     public void shouldTestPlaceHolders(String name, Integer seatInRow) {
-        String expected = "Hello Dear " + name +"!" + "\n" + "Your seat number is " + seatInRow;
+        String expected = "Hello Dear " + name + "!" + "\n" + "Your seat number is " + seatInRow;
         Assertions.assertEquals(expected, EmailGenerator.replacePlaceHoldersWithProvidedValue(name, seatInRow));
     }
 
@@ -23,5 +29,23 @@ class EmailGeneratorTest {
                 Arguments.of("Nika", 20),
                 Arguments.of("Eka", 21)
         );
+    }
+
+    @TestFactory
+    @DisplayName("should test if placeholders are replaced with provided values with Dynamic Tests")
+    Stream<DynamicTest> shouldTestPlaceholders() {
+        Map<String, Integer> userData = new HashMap<>();
+        userData.put("Lika", 19);
+        userData.put("Nika", 20);
+        userData.put("Eka", 21);
+
+        Set<Map.Entry<String, Integer>> entries = userData.entrySet();
+
+        return entries.stream().map(entry -> DynamicTest.dynamicTest(EmailGenerator.placeHolder, () ->
+                {
+                    String generated = EmailGenerator.replacePlaceHoldersWithProvidedValue(entry.getKey(), entry.getValue());
+                    Assertions.assertEquals(GeneratedEmailsOutputsMock.output.get(entry.getKey()), generated);
+                }
+        ));
     }
 }
---
+++ src/test/java/email/generator/hm/mock/GeneratedEmailsOutputsMock.java
@@ -0,0 +1,13 @@
+package email.generator.hm.mock;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class GeneratedEmailsOutputsMock {
+    public static Map<String, String> output = Map.of(
+            "Lika", "Hello Dear Lika!\nYour seat number is 19",
+            "Nika", "Hello Dear Nika!\nYour seat number is 20",
+            "Eka", "Hello Dear Eka!\nYour seat number is 21");
+
+}
