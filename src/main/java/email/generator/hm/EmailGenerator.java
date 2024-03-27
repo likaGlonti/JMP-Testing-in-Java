7f04cf23e53382db139cda1c4cf4119dcb473eba
VGLONLI <lika.glonti@pearson.com>
2024-03-27 18:22:47 +0400

    implement replacePlaceHoldersWithProvidedValue method

--- src/main/java/email/generator/hm/EmailGenerator.java
+++ src/main/java/email/generator/hm/EmailGenerator.java
@@ -1,8 +1,9 @@
 package email.generator.hm;
 
 public class EmailGenerator {
-    public static String replacePlaceHoldersWithProvidedValue(){
-        return "";
+    public static String replacePlaceHoldersWithProvidedValue(String name, Integer seat) {
+        return String.format(placeHolder, name, seat);
     }
 
+    public static String placeHolder = "Hello Dear %s!\nYour seat number is %d";
 }
--- src/test/java/email/generator/hm/EmailGeneratorTest.java
+++ src/test/java/email/generator/hm/EmailGeneratorTest.java
@@ -1,6 +1,7 @@
 package email.generator.hm;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,8 +11,10 @@ import java.util.stream.Stream;
 class EmailGeneratorTest {
     @ParameterizedTest
     @MethodSource("provideParameters")
-    public void shouldTestIfPlaceholdersAreReplacedWithProvidedValues(String name, Integer seatInRow) {
-        Assertions.assertTrue(EmailGenerator.replacePlaceHoldersWithProvidedValue() == "Hello Dear " + name + "your seating number is " + seatInRow);
+    @DisplayName("should test if placeholders are replaced with provided values with Parametrized Tests")
+    public void shouldTestPlaceHolders(String name, Integer seatInRow) {
+        String expected = "Hello Dear " + name +"!" + "\n" + "Your seat number is " + seatInRow;
+        Assertions.assertEquals(expected, EmailGenerator.replacePlaceHoldersWithProvidedValue(name, seatInRow));
     }
 
     private static Stream<Arguments> provideParameters() {
