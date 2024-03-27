4a6fe2561b386178bbe56be2cc6f0ff778951c3a
VGLONLI <lika.glonti@pearson.com>
2024-03-27 17:25:29 +0400

    make shouldTestIfPlaceholdersAreReplacedWithProvidedValues parametrized

--- src/test/java/email/generator/hm/EmailGeneratorTest.java
+++ src/test/java/email/generator/hm/EmailGeneratorTest.java
@@ -1,11 +1,24 @@
 package email.generator.hm;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class EmailGeneratorTest {
-    @Test
-    public void shouldTestIfPlaceholdersAreReplacedWithProvidedValues() {
-        Assertions.assertTrue(EmailGenerator.replacePlaceHoldersWithProvidedValue() == "Hello Dear mentee");
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    public void shouldTestIfPlaceholdersAreReplacedWithProvidedValues(String name, Integer seatInRow) {
+        Assertions.assertTrue(EmailGenerator.replacePlaceHoldersWithProvidedValue() == "Hello Dear " + name + "your seating number is " + seatInRow);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of("Lika", 19),
+                Arguments.of("Nika", 20),
+                Arguments.of("Eka", 21)
+        );
     }
 }
