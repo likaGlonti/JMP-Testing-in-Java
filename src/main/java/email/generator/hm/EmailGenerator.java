8b99e84da260509cd493b2e29ae57881372dc3d1
LikaGlonti <likaglonti20@gmail.com>
2024-03-27 23:54:43 +0400

    Revert "throw ArgumentMissingForMailGeneratorException if arguments are missing"
    This reverts commit 64f75fa65646cb9da2f1004804ff88d4baa40d2a.

--- src/main/java/email/generator/hm/EmailGenerator.java
+++ src/main/java/email/generator/hm/EmailGenerator.java
@@ -1,13 +1,9 @@
 package email.generator.hm;
 
-import email.generator.hm.exception.ArgumentMissingForMailGeneratorException;
-
 public class EmailGenerator {
-    public static String replacePlaceHoldersWithProvidedValue(String name, Integer seat) throws ArgumentMissingForMailGeneratorException {
-        if (name == null || name.isEmpty() || seat == null) {
-            throw new ArgumentMissingForMailGeneratorException("Arguments are missing, mail couldn't be generated");
-        }
+    public static String replacePlaceHoldersWithProvidedValue(String name, Integer seat) {
         return String.format(placeHolder, name, seat);
     }
+
     public static String placeHolder = "Hello Dear %s!\nYour seat number is %d";
 }
--- src/main/java/email/generator/hm/exception/ArgumentMissingForMailGeneratorException.java
+++
@@ -1,7 +0,0 @@
-package email.generator.hm.exception;
-
-public class ArgumentMissingForMailGeneratorException extends Exception {
-    public ArgumentMissingForMailGeneratorException (String errorMessage) {
-        super(errorMessage);
-    }
-}
