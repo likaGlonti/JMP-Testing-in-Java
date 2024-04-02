a022f9ea2aeceec7a52886c6e51f46102d9bf99e
likaGlonti <likaGlonti20@gmail.com>
2024-04-02 19:21:14 +0400

    create test to check if MailServer executes send method

--- src/test/java/email/generator/hm/MessengerTest.java
+++ src/test/java/email/generator/hm/MessengerTest.java
@@ -3,7 +3,7 @@ package email.generator.hm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class MessengerTest {
     @Test
@@ -16,5 +16,13 @@ public class MessengerTest {
     public void shouldCreateMailServer(){
         MailServer server = new MailServer();
         Assertions.assertInstanceOf(MailServer.class, server);
+    }
+
+    @Test
+    public void shouldTestIfMailServerSendsProvidedMail(){
+        MailServer server = mock(MailServer.class);
+        Messenger messenger = new Messenger();
+        messenger.sendMail();
+        verify(server).send();
     }
 }
