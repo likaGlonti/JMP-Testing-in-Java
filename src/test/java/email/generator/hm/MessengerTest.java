f2518a414f9f1e5756f68089e1f72acba2997b75
likaGlonti <likaGlonti20@gmail.com>
2024-04-02 19:12:05 +0400

    add test to check if messenger is created

---
+++ src/test/java/email/generator/hm/MessengerTest.java
@@ -0,0 +1,15 @@
+package email.generator.hm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class MessengerTest {
+    @Test
+    public void shouldCreateMessenger() {
+        Messenger messenger = mock(Messenger.class);
+
+        Assertions.assertInstanceOf(Messenger.class, messenger);
+    }
+}
--- src/test/java/email/generator/hm/mock/GeneratedEmailsOutputsMock.java
+++ src/test/java/email/generator/hm/mock/GeneratedEmailsOutputsMock.java
@@ -1,7 +1,5 @@
 package email.generator.hm.mock;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 public class GeneratedEmailsOutputsMock {
